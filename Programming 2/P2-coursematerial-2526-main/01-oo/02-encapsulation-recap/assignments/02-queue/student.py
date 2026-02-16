7b72aee21b6b7015d4d88b333a1a8759a38c8c5f
Benjamin <siffertbenjamin@gmail.com>
2026-02-16 10:05:21 +0100

    python oefeningetjes wat gedaan

--- Programming 2/P2-coursematerial-2526-main/01-oo/02-encapsulation-recap/assignments/02-queue/student.py
+++ Programming 2/P2-coursematerial-2526-main/01-oo/02-encapsulation-recap/assignments/02-queue/student.py
@@ -1,12 +1,28 @@
 class Queue():
     def __init__(self):
-        self.items = []
+        self.__items = []
     
     def add(self, item):
-        self.items.append(item)
-
-
+        self.__items.append(item)
+    def is_empty(self):
+        if len(self.__items) == 0:
+            return (False)
+        else:
+            return (True)            
+    def next(self):
+        self.__items.pop(-1)
+        new_list = self.get__items()
+        print(new_list[-1] + " Is next!")
+        return new_list[-1]
+    def get__items(self):
+        return self.__items
+    def set__items(self, items):
+        self.__items = items
+        
 queue = Queue()
+print(queue.is_empty())
+queue.add("jip")
+queue.add("janneke")
+queue.is_empty()
 
-queue.add('Alice')
-print(queue)
+
